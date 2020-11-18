a557bf667b68287680e164bc1c966c384e809a45
Elnur Mammadli <elnoor@users.noreply.github.com>
2020-11-17 22:38:00 -0700

    Update index.js
    whole logic redesigned; function used instead of class; more cases covered

--- index.js
+++ index.js
@@ -1,80 +1,69 @@
-class Suffix {
-  suffix(year) {
-    this.year = year.toString();
+export default function azNumSuffix(num, concatStr = null) {
+  const strNum = num.toString().trim();
+  if (isNaN(num) || strNum.length === 0) return num;
+  let suffix = "";
+  if (strNum.endsWith("000000"))
+    // milyonuncu
+    suffix = "cu";
+  else if (strNum.endsWith("000"))
+    //mininci
+    suffix = "ci";
 
-    if (this.year.length > 0) {
-      this.prefix = this.year.slice(this.year.length - 1);
+  if (!suffix) {
+    const lastTwoSymbols = strNum.substring(strNum.length - 2);
+    if (lastTwoSymbols.length === 2 && !lastTwoSymbols.includes(".")) {
+      switch (lastTwoSymbols) {
+        case "00": // yuzuncu
+          suffix = "cü";
+          break;
+        case "10":
+        case "30":
+          suffix = "cu";
+          break;
+        case "20":
+        case "50":
+        case "70":
+        case "80":
+          suffix = "ci";
+          break;
+        case "40":
+        case "60":
+        case "90":
+          suffix = "cı";
+          break;
+        default:
+          break;
+      }
     }
 
-    switch (this.prefix) {
-      case '0':
-      case '6':
-        this.suffix = 'cı';
-        break;
-
-      case '1':
-      case '2':
-      case '5':
-      case '7':
-      case '8':
-        this.suffix = 'ci';
-        break;
-
-      case '3':
-      case '4':
-        this.suffix = 'cü';
-        break;
-
-      case '9':
-        this.suffix = 'cu';
-        break;
-
-      default:
-        break;
+    if (!suffix) {
+      const lastSymbol = strNum.substring(strNum.length - 1);
+      switch (lastSymbol) {
+        case "0":
+        case "6":
+          suffix = "cı";
+          break;
+        case "1":
+        case "2":
+        case "5":
+        case "7":
+        case "8":
+          suffix = "ci";
+          break;
+        case "3":
+        case "4":
+          suffix = "cü";
+          break;
+        case "9":
+          suffix = "cu";
+          break;
+        default:
+          break;
+      }
     }
-
-    if (this.year.length > 1) {
-      this.prefix = this.year.slice(this.year.length - 2);
-    }
-
-    switch (this.prefix) {
-      case '10':
-      case '30':
-        this.suffix = 'cu';
-        break;
-
-      case '20':
-      case '40':
-      case '50':
-      case '60':
-      case '70':
-      case '80':
-      case '90':
-        this.suffix = 'ci';
-        break;
-
-      default:
-        break;
-    }
-
-    this.value = this.suffix;
-
-    return this;
   }
 
-  symbol() {
-    this.value = `- ${this.value}`;
-
-    return this;
-  }
-
-  symbolYear() {
-    this.value += ` il`;
-
-    return this;
-  }
-
-  get() {
-    return `${this.year} ${this.value}`;
-  }
+  let result = `${strNum}-${suffix}`;
+  if (concatStr) result = `${result} ${concatStr}`;
+  return result;
 }
